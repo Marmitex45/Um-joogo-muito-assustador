2a23fd327ffae908d2641bc4e9ece105e5361439
Marmitex45 <fabrica.de.nada2310@gmail.com>
2026-04-15 17:13:55 -0300

    bala

--- jogo-assustador/scripts/jogador.gd
+++ jogo-assustador/scripts/jogador.gd
@@ -1,5 +1,18 @@
 extends CharacterBody2D
 var velocidade = PlayerData.velocidade
+var listacoraçao : Array[TextureRect]
+var vida = PlayerData.vida
+@export var projetil : PackedScene
+@onready var ponta_arma = $Emissorbala
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("Atirar"):
+		disparar()
+
+func disparar():
+	var nova_bala = projetil.instantiate()
+	nova_bala.global_position = ponta_arma.global_position
+	get_tree().current_scene.add_child(nova_bala)
 
 func _physics_process(delta):
 	var direcao = Input.get_vector("Esquerda", "Direita", "Cima", "Baixo")
@@ -7,3 +20,10 @@ func _physics_process(delta):
 	move_and_slide()
 	if direcao.x != 0:
 		$AnimatedSprite2D.flip_h = direcao.x < 0
+func tomar_dano():
+	atualizar_coraçoes()
+	
+		
+func atualizar_coraçoes():
+	for i in range(listacoraçao.size()):
+		listacoraçao[i].visible = 1 < vida
